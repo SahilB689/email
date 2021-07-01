fc2ab87c7b37e24e82a1540d803f2b691dc873a0
SahilB689 <76981086+SahilB689@users.noreply.github.com>
2021-07-01 10:39:54 -0400

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -113,3 +113,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+    
+
