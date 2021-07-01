421cec096031021f9a0a7082dbbef8a412c5e6fc
SahilB689 <76981086+SahilB689@users.noreply.github.com>
2021-07-01 10:38:29 -0400

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -1,6 +1,6 @@
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -111,6 +111,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+django_heroku.settings(locals())
+
